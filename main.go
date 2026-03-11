d0f3d01182c1dcba46a17e893f2b2715eedef5f0
taterbase <shankga@gmail.com>
2026-03-11 13:12:37 -0400

    Suspended "head" of file
    Escrito now dumps as much of the file that will fit in the current
    terminal height while staying suspended (not exiting).
    
    Now I need to be able to scroll the file... which will require
    "listening" to key presses. I think this will require putting the
    terminal into raw mode which will mean printing differently to the
    terminal as well since when printing strings in raw mode seems to almost
    "hold on" to past tabs? not sure, more research needed.

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
+	"sync"
+	"time"
 
 	"golang.org/x/term"
 )
 
-var usageMsg = `
+var (
+	usageMsg = `
 Example: esc file.go
 `
+
+	wg sync.WaitGroup
+)
 
 func main() {
 	// Boilerplate from https://pkg.go.dev/golang.org/x/term#pkg-overview
@@ -19,32 +27,31 @@ func main() {
 	//if err != nil {
 	//	handleError(err)
 	//}
-	// Return to "cooked" terminal
+	//// Return to "cooked" terminal
 	//defer term.Restore(int(os.Stdin.Fd()), oldState)
 
 	//TODO: holding off on raw until we need it. Right now we're just a basic
 	//			viewer.
 
-	if len(os.Args) < 2 {
+	if len(os.Args) != 2 {
 		usage()
 		return
 	}
 	filename := (os.Args[1])
-	file, err := os.Open(filename)
-	if err != nil {
-		handleError(err)
-	}
-
-	_, err = os.Stdout.ReadFrom(file)
-	if err != nil {
-		handleError(err)
-	}
 
 	w, h, err := term.GetSize(int(os.Stdin.Fd()))
 	if err != nil {
 		handleError(err)
 	}
-	fmt.Println(w, h)
+
+	editor := NewEditor(w, h)
+	err = editor.OpenFile(filename)
+	if err != nil {
+		handleError(err)
+	}
+	wg.Add(1)
+	editor.Display()
+	wg.Wait()
 }
 
 // Teach'em son
@@ -54,5 +61,64 @@ func usage() {
 
 // For now my sweet child
 func handleError(err error) {
-	panic(err)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type File struct {
+	raw      *os.File
+	contents []string // starting with a slice of strings... rough
+}
+
+type Editor struct {
+	// Width and height of the editor (the terminal window)
+	width  int
+	height int
+
+	// The file currently being worked on. Only supports
+	// one file for now. How quaint!
+	file *File
+}
+
+func NewEditor(w, h int) *Editor {
+	return &Editor{
+		width:  w,
+		height: h,
+	}
+}
+
+func (e *Editor) OpenFile(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("Error reading file: %w", err)
+	}
+
+	lines := strings.Split(string(contents), "\n")
+
+	e.file = &File{
+		raw:      file,
+		contents: lines,
+	}
+	return nil
+}
+
+func (e *Editor) Display() {
+	t := time.NewTicker(30 * time.Second)
+	for {
+		fmt.Print(e.file.contents[0])
+		for i := 1; i < e.height; i++ {
+			fmt.Print("\n" + e.file.contents[i])
+		}
+
+		select {
+		case <-t.C:
+			continue
+		}
+	}
 }
