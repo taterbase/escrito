0925d7e12242ab50a4f0caf517ca651bae36d6b2
taterbase <shankga@gmail.com>
2026-03-11 21:33:18 -0400

    Rough experiment
    Some code to introduce a "raw" mode that still allows for signals like
    kill to stop program as well as keeping output processing.
    
    Output processing probably doesn't need to be kept as long as I maintain
    all my carriage returns and line feeds... but we'll see. For now I
    definitely want to let the os handle these program ending signals.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 	"time"
 
+	"golang.org/x/sys/unix"
 	"golang.org/x/term"
 )
 
@@ -19,16 +20,51 @@ Example: esc file.go
 	wg sync.WaitGroup
 )
 
+const ioctlReadTermios = unix.TCGETS
+const ioctlWriteTermios = unix.TCSETS
+
+type state struct {
+	termios unix.Termios
+}
+
+func rawDog(fd int) error {
+	termios, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
+	if err != nil {
+		return err
+	}
+
+	// This is copy/paste job of the make raw behavior of the term lib from
+	// golang.org/x/term but with some modifications by me.
+	termios.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK | unix.ISTRIP | unix.INLCR | unix.IGNCR | unix.ICRNL | unix.IXON
+	//termios.Oflag &^= unix.OPOST
+	//termios.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON | unix.ISIG | unix.IEXTEN
+	termios.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON | unix.IEXTEN
+	termios.Cflag &^= unix.CSIZE | unix.PARENB
+	termios.Cflag |= unix.CS8
+	termios.Cc[unix.VMIN] = 1
+	termios.Cc[unix.VTIME] = 0
+	if err := unix.IoctlSetTermios(fd, ioctlWriteTermios, termios); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	// Boilerplate from https://pkg.go.dev/golang.org/x/term#pkg-overview
 	// Raw mode let's us worry about terminal sequences ourselves instead of
 	// the terminal handling them.
-	//oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
-	//if err != nil {
-	//	handleError(err)
-	//}
+	oldState, err := term.GetState(int(os.Stdin.Fd()))
+	if err != nil {
+		handleError(err)
+	}
+
+	err = rawDog(int(os.Stdin.Fd()))
+	if err != nil {
+		handleError(err)
+	}
 	//// Return to "cooked" terminal
-	//defer term.Restore(int(os.Stdin.Fd()), oldState)
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
 
 	//TODO: holding off on raw until we need it. Right now we're just a basic
 	//			viewer.
@@ -109,10 +145,16 @@ func (e *Editor) OpenFile(filename string) error {
 }
 
 func (e *Editor) Display() {
-	t := time.NewTicker(30 * time.Second)
+	t := time.NewTicker(1 * time.Second)
 	for {
+		_, h, err := term.GetSize(int(os.Stdin.Fd()))
+		if err != nil {
+			handleError(err)
+		}
+		fmt.Print("\033[2J")
+		fmt.Print("\033[H")
 		fmt.Print(e.file.contents[0])
-		for i := 1; i < e.height; i++ {
+		for i := 1; i < h; i++ {
 			fmt.Print("\n" + e.file.contents[i])
 		}
 
